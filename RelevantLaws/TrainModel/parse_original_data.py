202c1fbb18609c484d08668828b059ab0d0684ac
zhutaonan <adolf1321794021@gmail.com>
2022-03-23 09:12:10 +0800

    update

--- RelevantLaws/TrainModel/parse_original_data.py
+++ RelevantLaws/TrainModel/parse_original_data.py
@@ -5,22 +5,51 @@
 # @Site    : 
 # @File    : parse_original_data.py
 # @Software: PyCharm
+import pandas as pd
 from docx import Document
+import re
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
 
 
 # 读取docx 文件
-def read_docx_file(docx_path):
-    document = Document(docx_path)
+def parse_docx_to_csv(origin_doc):
+    document = Document(origin_doc)
     all_paragraphs = document.paragraphs
 
-    return_text_list = []
+    parts = []
+    chapters = []
+    items = []
+    contents = []
+
+    current_part = ""
+    current_chapter = ""
 
     for index, paragraph in enumerate(all_paragraphs):
-        one_text = paragraph.text.replace(" ", "").replace("\u3000", "")
-        if one_text != "":
-            return_text_list.append(one_text)
-    # print(return_text_list)
-    return return_text_list
+        # one_text = paragraph.text.replace(" ", "").replace("\u3000", "")
+        one_text = paragraph.text.strip().replace('\u3000', " ")
+        # print(one_text)
+        if len(re.findall('第*编', one_text[:3])) > 0:
+            current_part = one_text
+            # print(current_part)
+        if len(re.findall('第*章', one_text[:3])) > 0:
+            current_chapter = one_text
+
+        if len(re.findall('第*条', one_text[:3])) > 0:
+            parts.append(current_part)
+            chapters.append(current_chapter)
+            items.append(one_text)
+            contents.append(" ")
+        # break
+
+    res_dict = {"总编": parts,
+                "章节": chapters,
+                "条目": items,
+                "内容": contents}
+    res_df = pd.DataFrame(res_dict)
+    # print(res_df)
+    res_df.to_csv("RelevantLaws/LawsData/民法典.csv", index=False)
 
 
-read_docx_file(docx_path="DocumentReview/DocData/Sample/安保服务外包合同-甲方.docx")
+parse_docx_to_csv(origin_doc="RelevantLaws/LawsData/民法典.docx")
