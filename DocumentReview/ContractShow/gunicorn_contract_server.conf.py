95ea08b27a64a09b6d47195f462503fdad43fff6
inamori1932 <inamori1932@qq.com>
2022-08-02 17:32:33 +0800

    修改合同智审服务的配置文件。

--- DocumentReview/ContractShow/gunicorn_contract_server.conf.py
+++ DocumentReview/ContractShow/gunicorn_contract_server.conf.py
@@ -7,7 +7,7 @@ daemon = True
 # 设置超时时间120s，默认为30s。按自己的需求进行设置
 timeout = 300
 #Restart workers when code changes. False:will not restart
-reload = True
+reload = False
 # 设置访问日志和错误信息日志路径
 accesslog = 'log/document_review/gunicorn_contract_server_access.log'
 errorlog = 'log/document_review/gunicorn_contract_server_error.log'
