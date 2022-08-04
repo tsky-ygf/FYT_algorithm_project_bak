e97cb0f0b6f132d898a4ef55eadb4c29340d5dbc
inamori1932 <inamori1932@qq.com>
2022-08-04 16:43:25 +0800

    修改了特征匹配的测试用例。

--- LawsuitPrejudgment/tests/nlu/factor_extractors/test_regex_factor_extractor.py
+++ LawsuitPrejudgment/tests/nlu/factor_extractors/test_regex_factor_extractor.py
@@ -9,8 +9,23 @@ from LawsuitPrejudgment.common import single_case_match
 
 
 def test_single_case_match():
+    """
+        发现:
+            考虑"借款人死亡"这个特征。对比【[借贷][死]】和【[借贷]】【[死]】
+            1)"借"和"去世"直接没有标点符号，两种模式都能匹配出来。
+            fact = "我朋友向我借了二十万块钱后来他去世了，我像大孩子要钱"
+            2)用句号分割。
+            【[A]】【[B]】未考虑多句(。等分隔)的匹配。
+            对于A。B。两种模式都匹配不出来。
+            fact = "我朋友向我借了二十万块钱。后来他去世了，我像大孩子要钱"
+            3)用逗号分割。
+            【[A][B]】未考虑句内(，等分割)的匹配。
+            对于A，B。【[A][B]】匹配不出来。【[A]】【[B]】能匹配出来。
+            # fact = "我朋友向我借了二十万块钱，后来他去世了。我像大孩子要钱"
+
+            相应的处理逻辑，在config_loader.get_factor_positive_keyword()
+    """
     problem = "借贷纠纷"
-    # fact = "我朋友向我借了二十万块钱，后来他去世了，我像大孩子要钱"
     fact = "我朋友向我借了二十万块钱，后来他去世了。我像大孩子要钱"
 
     result = single_case_match(fact, problem, None)
@@ -18,5 +33,5 @@ def test_single_case_match():
     print("#"*50)
     print(result)
     assert result
-    assert "借款人借款后死亡" in result
-    assert result["借款人借款后死亡"][1] == 1
+    assert "借款人借款后死亡" in result or "借款人死亡" in result
+    assert result["借款人借款后死亡"][1] == 1 or result["借款人死亡"][1] == 1
