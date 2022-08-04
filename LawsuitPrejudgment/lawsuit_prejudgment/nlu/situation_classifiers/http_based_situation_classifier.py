70afa5b0ceacb62ea4062d8f3882cd3b8c1bfa8d
inamori1932 <inamori1932@qq.com>
2022-08-04 17:02:50 +0800

    修改情形分类模块的HttpClient。

--- LawsuitPrejudgment/lawsuit_prejudgment/nlu/situation_classifiers/http_based_situation_classifier.py
+++ LawsuitPrejudgment/lawsuit_prejudgment/nlu/situation_classifiers/http_based_situation_classifier.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import traceback
 
@@ -10,17 +11,19 @@ from LawsuitPrejudgment.lawsuit_prejudgment.nlu.situation_classifiers.situation_
 
 class HttpClient:
     def __init__(self, url, method="post"):
-        if method not in ["post", "get"]:
-            raise Exception("method应是post或get。传入了不支持的method:{}。".format(method))
         self.url = url
         self.method = method
 
     def get_response_json(self, request_data: Dict):
         try:
             if self.method == "post":
-                return requests.post(url=self.url, json=request_data).json()
-            elif self.method == "get":
-                return requests.get(url=self.url, params=request_data).json()
+                resp_json = requests.post(url=self.url, json=request_data).json()
+            elif self.method == 'get':
+                resp_json = requests.get(url=self.url, params=request_data).json()
+            else:
+                raise Exception("method应是post或get。传入了不支持的method:{}。".format(self.method))
+
+            return json.loads(resp_json) if isinstance(resp_json, str) else resp_json
         except Exception:
             logging.exception(traceback.format_exc())
             return dict()
