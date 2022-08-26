7f00873b896766bb28c2884c6e166750e52c0fb2
adolf <adolf1321794021@gmail.com>
2022-08-26 11:37:37 +0800

    刑事更新

--- LawsuitPrejudgment/Criminal/basic_prejudgment.py
+++ LawsuitPrejudgment/Criminal/basic_prejudgment.py
@@ -48,10 +48,10 @@ class PrejudgmentPipeline:
 
     def __call__(self, *args, **kwargs):
         self.content.update(kwargs)
+        self.parse_xmind()
         self.anyou_identify()
         self.suqiu_identify()
         self.situation_identify()
-        self.parse_xmind()
         self.get_question()
         self.generate_report()
 
