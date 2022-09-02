a7c5ea339a6396fca534651a90fc7312ed0e6829
inamori1932 <inamori1932@qq.com>
2022-09-02 13:16:06 +0800

    修改gunicorn配置

--- RelevantLaws/api/gunicorn_relevant_laws_api.conf.py
+++ RelevantLaws/api/gunicorn_relevant_laws_api.conf.py
@@ -7,7 +7,7 @@ daemon = True
 # 设置超时时间120s，默认为30s。按自己的需求进行设置
 timeout = 120
 #Restart workers when code changes. False:will not restart
-reload = True
+reload = False
 # 设置访问日志和错误信息日志路径
 accesslog = 'log/relevant_laws/gunicorn_relevant_laws_api_access.log'
 errorlog = 'log/relevant_laws/gunicorn_relevant_laws_api_error.log'
