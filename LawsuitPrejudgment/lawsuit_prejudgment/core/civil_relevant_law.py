e831bf2532215f7ee112942f31a77580f3edf8a8
inamori1932 <inamori1932@qq.com>
2022-09-03 15:53:41 +0800

    修改民事预判的相似案例、相关法条。

---
+++ LawsuitPrejudgment/lawsuit_prejudgment/core/civil_relevant_law.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" 
+@Author  : inamori1932
+@Time    : 3/9/2022 12:13 
+@Desc    : None
+"""
+class CivilRelevantLaw:
+    def __init__(self):
+        pass
