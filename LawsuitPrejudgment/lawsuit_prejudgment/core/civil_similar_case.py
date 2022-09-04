049dee7ef2b189154e2bcd56f880d0247a383bcd
inamori1932 <inamori1932@qq.com>
2022-09-04 12:58:29 +0800

    适配第三方的参数问题：处理了factor_sentence_list参数的异常情况。

--- LawsuitPrejudgment/lawsuit_prejudgment/core/civil_similar_case.py
+++ LawsuitPrejudgment/lawsuit_prejudgment/core/civil_similar_case.py
@@ -5,3 +5,21 @@
 @Time    : 3/9/2022 12:12 
 @Desc    : None
 """
+import requests
+
+
+class CivilSimilarCase:
+    def __init__(self, fact, problem, claim_list):
+        self.fact = fact
+        self.problem = problem
+        self.claim_list = claim_list
+
+    def get_similar_cases(self):
+        url = "http://172.19.82.198:5011/top_k_similar_narrative"
+        body = {
+            "fact": self.fact,
+            "problem": self.problem,
+            "claim_list": self.claim_list
+        }
+        resp_json = requests.post(url, json=body).json()
+        return resp_json
