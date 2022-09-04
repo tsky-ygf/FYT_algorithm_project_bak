1e99b4e34e76f8f39abde68b744875d72132a601
inamori1932 <inamori1932@qq.com>
2022-09-04 18:10:57 +0800

    处理了热门标签的问答。

--- IntelligentConsultation/core/intelligent_consultation_service.py
+++ IntelligentConsultation/core/intelligent_consultation_service.py
@@ -6,9 +6,30 @@
 @Desc    : None
 """
 import requests
+import pandas as pd
+
+
+df_individual = pd.read_csv("IntelligentConsultation/core/个人端热门标签问答.csv", encoding="utf-8")
+df_company = pd.read_csv("IntelligentConsultation/core/企业端热门标签问答.csv", encoding="utf-8")
+
+
+def _get_initial_answer(question: str):
+    for index, row in df_individual.iterrows():
+        if str(row["问题"]).strip() == str(question).strip():
+            return str(row["回答"])
+    for index, row in df_company.iterrows():
+        if str(row["问题"]).strip() == str(question).strip():
+            return str(row["回答"])
+    return None
 
 
 def get_query_answer(question: str):
+    initial_answer = _get_initial_answer(question)
+    if initial_answer:
+        return {
+            "answer": initial_answer
+        }
+
     url = "http://172.19.82.198:5050/get_query_answer"
     resp_json = requests.post(url, json={"question":question}).json()
     return {
