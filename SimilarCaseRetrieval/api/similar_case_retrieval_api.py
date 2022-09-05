ab2e4789fac4f9a0e6fd07b59c07e5f674d53df7
hyl2048 <2745506937@qq.com>
2022-09-05 16:10:19 +0800

    相似案例更新

--- SimilarCaseRetrieval/api/similar_case_retrieval_api.py
+++ SimilarCaseRetrieval/api/similar_case_retrieval_api.py
@@ -147,4 +147,4 @@ def _construct_result_format(search_result) -> List:
     return result
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8152, debug=True)
+    app.run(host="0.0.0.0", port=8140, debug=True)
