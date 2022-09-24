ead61919130e4f0f0bf6f21f72d3b2c3e0e8c53b
inamori1932 <inamori1932@qq.com>
2022-09-24 23:35:13 +0800

    将合同智审的接口，以多线程+回调后端接口的方式，修改为异步的。

--- DocumentReview/ContractShow/contract_server_show.py
+++ DocumentReview/ContractShow/contract_server_show.py
@@ -7,10 +7,14 @@
 # @Software: PyCharm
 import time
 import traceback
+from typing import Dict
+
+import requests
 from flask import Flask, request
 from flask_cors import CORS
 import json
 from loguru import logger
+from concurrent.futures import ThreadPoolExecutor
 
 from DocumentReview.ContractReview.showing_sample import BasicUIEAcknowledgementShow
 import os
@@ -62,51 +66,61 @@ def get_user_standpoint():
     return response_successful_result(user_standpoints)
 
 
+# 创建线程执行器
+executor = ThreadPoolExecutor(10)
+
+
 @app.route('/get_contract_review_result', methods=["post"])
 def get_contract_review_result():
     try:
         in_json = request.get_data()
         if in_json is not None:
-            in_dict = json.loads(in_json.decode("utf-8"))
-            contract_type_id = in_dict['contract_type_id']
-            text = in_dict['contract_content']
-            usr = in_dict['user_standpoint_id']
-            # print(text)
-            # print(contract_type)
-            if usr == 'party_a':
-                usr = 'Part A'
-            elif usr == 'part_b':
-                usr = 'Part B'
-            else:
-                raise Exception("暂时不支持该用户立场")
-
-            acknowledgement = acknowledgement_dict[contract_type_id]
-
-            acknowledgement.review_main(content=text, mode="text", usr=usr)
-            res = acknowledgement.review_result
-            origin_data = acknowledgement.data
-            # 编排返回结果的内容
-            result = []
-            for review_point, review_result in res.items():
-                result.append({
-                    "review_point": review_point,
-                    "show_name": review_result.get("show name") if review_result.get("show name") else review_point,
-                    "review_result": review_result.get("审核结果", ""),
-                    "review_content": review_result.get("内容", ""),
-                    "review_content_start": review_result.get("start", -1),
-                    "review_content_end": review_result.get("end", -1),
-                    "legal_advice": review_result.get("法律建议", ""),
-                    "legal_basis": review_result.get("法律依据", ""),
-                    "risk_level": review_result.get("风险等级", ""),
-                    "risk_point": review_result.get("风险点", "")
-                })
-            body = {"success": True, "error_msg": "", "result": result, 'origin_data': origin_data}
-            return json.dumps(body, ensure_ascii=False)
+            request_data = json.loads(in_json.decode("utf-8"))
+            # 调用线程池执行器执行异步任务
+            executor.submit(_async_task, request_data)
+            return response_successful_result(None)
         else:
-            return json.dumps({"error_msg": "no data", "status": 1}, ensure_ascii=False)
+            return response_failed_result("no data")
     except Exception as e:
         logger.info(traceback.format_exc())
         return response_failed_result(traceback.format_exc())
+
+
+def _async_task(request_data: Dict):
+    unique_id = request_data['id']
+    contract_type_id = request_data['contract_type_id']
+    text = request_data['contract_content']
+    usr = request_data['user_standpoint_id']
+
+    if usr == 'party_a':
+        usr = 'Part A'
+    elif usr == 'part_b':
+        usr = 'Part B'
+    else:
+        raise Exception("暂时不支持该用户立场")
+
+    acknowledgement = acknowledgement_dict[contract_type_id]
+
+    acknowledgement.review_main(content=text, mode="text", usr=usr)
+    res = acknowledgement.review_result
+    # 编排返回结果的内容
+    result = []
+    for review_point, review_result in res.items():
+        result.append({
+            "review_point": review_point,
+            "show_name": review_result.get("show name") if review_result.get("show name") else review_point,
+            "review_result": review_result.get("审核结果", ""),
+            "review_content": review_result.get("内容", ""),
+            "review_content_start": review_result.get("start", -1),
+            "review_content_end": review_result.get("end", -1),
+            "legal_advice": review_result.get("法律建议", ""),
+            "legal_basis": review_result.get("法律依据", ""),
+            "risk_level": review_result.get("风险等级", ""),
+            "risk_point": review_result.get("风险点", "")
+        })
+    body = {"id": unique_id, "htZsList": result}
+    requests.post(url="http://192.168.0.81:8092/serve/HtZs/notify", json=body)
+    pass
 
 
 if __name__ == '__main__':
