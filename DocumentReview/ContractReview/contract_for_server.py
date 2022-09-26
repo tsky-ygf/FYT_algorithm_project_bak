1b3e093cc097adfe5e710dc58d6b3e89907e9bca
adolf <adolf1321794021@gmail.com>
2022-09-26 09:57:36 +0800

    update

---
+++ DocumentReview/ContractReview/contract_for_server.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/9/26 09:43
+# @Author  : Adolf
+# @Site    : 
+# @File    : contract_for_server.py
+# @Software: PyCharm
+import json
+from DocumentReview.ContractReview.showing_sample import BasicUIEAcknowledgementShow
+
+CONTRACT_SERVER_DATA_PATH = "DocumentReview/Config/contract_server_data.json"
+
+
+def get_support_contract_types():
+    with open(CONTRACT_SERVER_DATA_PATH, "r", encoding="utf-8") as f:
+        return json.load(f).get("support_contract_types")
+
+
+def get_user_standpoint():
+    with open(CONTRACT_SERVER_DATA_PATH, "r", encoding="utf-8") as f:
+        user_standpoints = json.load(f).get("user_standpoints")
+    return user_standpoints
+
+
+def get_contract_type_list():
+    support_contract_types = get_support_contract_types()
+    return [item.get("type_id") for item in support_contract_types]
+
+
+def init_model():
+    contract_type_list = get_contract_type_list()
+    acknowledgement_dict = {}
+    for contract_type in contract_type_list:
+        config_path = "DocumentReview/Config/{}.csv".format(contract_type)
+        model_path = "model/uie_model/export_cpu/{}/inference".format(contract_type)
+        acknowledgement_dict[contract_type] = BasicUIEAcknowledgementShow(config_path=config_path,
+                                                                          model_path=model_path,
+                                                                          device="cpu")
+    return acknowledgement_dict
--- OnlineServer/ContractReview/server.py
+++ OnlineServer/ContractReview/server.py
@@ -5,3 +5,38 @@
 # @Site    : 
 # @File    : server.py
 # @Software: PyCharm
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from DocumentReview.ContractReview.contract_for_server import *
+
+app = FastAPI()
+
+init_model()
+
+
+@app.get('/get_contract_type')
+def _get_support_contract_types():
+    return {"result": get_support_contract_types()}
+
+
+@app.get('/get_user_standpoint')
+def _get_user_standpoint():
+    return {"result": get_user_standpoint()}
+
+
+class Item(BaseModel):
+    text: str = "何谓耐受性？"
+
+
+@app.post("/administrative_consult")
+async def get_other_consult(item: Item):
+    print("input item: ", item)
+    answer = faq_predict(item.text)
+    return {"result": answer}
+
+
+if __name__ == "__main__":
+    # 日志设置
+    uvicorn.run('ProfessionalSearch.Consult.FAQ_server:app', host="0.0.0.0", port=8110, reload=False, workers=4)
