88625d0ec74f2f024e68d41987816e2c60e6337e
adolf <adolf1321794021@gmail.com>
2022-09-29 14:24:34 +0800

    update

--- OnlineServer/ProfessionalSearch/server.py
+++ OnlineServer/ProfessionalSearch/server.py
@@ -20,10 +20,12 @@ from Utils.io import read_json_attribute_value
 
 app = FastAPI()
 
+
 class Filter_conditions_law(BaseModel):
     timeliness: list
     types_of_law: list
     scope_of_use: list
+
 
 class Search_laws_input(BaseModel):
     query: str
@@ -31,8 +33,10 @@ class Search_laws_input(BaseModel):
     page_number: int
     page_size: int
 
+
 class Law_id(BaseModel):
     law_id: str
+
 
 class Filter_conditions_case(BaseModel):
     type_of_case: list
@@ -40,40 +44,51 @@ class Filter_conditions_case(BaseModel):
     type_of_document: list
     region: list
 
+
 class Search_cases_input(BaseModel):
     query: str
     filter_conditions: Union[Filter_conditions_case, None] = None
     page_number: int
     page_size: int
 
+
 class Case_id(BaseModel):
     case_id: str
+
 
 @app.get('/get_filter_conditions_of_law')
 def _get_filter_conditions():
     return {"result": get_filter_conditions()}
 
+
 @app.post('/search_laws')
 def search_laws(search_query: Search_laws_input):
-    return {"result": _get_law_result(search_query.query, search_query.filter_conditions, search_query.page_number, search_query.page_size)}
+    return {"result": _get_law_result(search_query.query, search_query.filter_conditions, search_query.page_number,
+                                      search_query.page_size)}
+
 
 @app.get('/get_law_by_law_id')
 def get_law_by_law_id(law_id: Law_id):
     # TODO 待更新
     return {"result": ""}
 
+
 @app.get('/get_filter_conditions_of_case')
 def _get_filter_conditions():
     return {"result": get_filter_conditions_of_case()}
 
+
 @app.post('/search_cases')
 def search_laws(search_query: Search_cases_input):
-    return {"result": _get_case_result(search_query.query, search_query.filter_conditions, search_query.page_number, search_query.page_size)}
+    return {"result": _get_case_result(search_query.query, search_query.filter_conditions, search_query.page_number,
+                                       search_query.page_size)}
+
 
 @app.get('/get_law_document')
 def get_law_document(case_id: Case_id):
     # TODO 待更新
     return {"result": ""}
+
 
 if __name__ == "__main__":
     # 日志设置
