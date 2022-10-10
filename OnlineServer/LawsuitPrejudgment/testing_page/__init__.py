73c65bbabe7fb2893dd2fada9f715a64804a8db9
inamori1932 <inamori1932@qq.com>
2022-10-10 14:31:42 +0800

    将诉讼预判的测试页面，集中到一个文件(OnlineServer/LawsuitPrejudgment/show.py)中。

--- OnlineServer/LawsuitPrejudgment/testing_page/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-""" 
-@Author  : inamori1932
-@Time    : 28/9/2022 17:14 
-@Desc    : None
-"""
--- OnlineServer/LawsuitPrejudgment/testing_page/civil/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-""" 
-@Author  : inamori1932
-@Time    : 30/9/2022 13:53 
-@Desc    : None
-"""
