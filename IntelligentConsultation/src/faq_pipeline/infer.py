b61de68511e249ea9d7d9fad22afeaf7f9b0d04f
adolf <adolf1321794021@gmail.com>
2022-10-13 14:06:13 +0800

    update

--- IntelligentConsultation/src/faq_pipeline/infer.py
+++ IntelligentConsultation/src/faq_pipeline/infer.py
@@ -5,10 +5,9 @@
 # @Site    : 
 # @File    : infer.py
 # @Software: PyCharm
+from jieba import analyse
+
 from IntelligentConsultation.src.faq_pipeline.init_faq_tools import init_haystack_fqa_pipe
-
-from haystack.pipelines import FAQPipeline
-
 from Utils.logger import get_logger
 
 
@@ -22,27 +21,39 @@ class FAQPredict:
         self.logger = get_logger(level=level, console=console, logger_file=logger_file)
         self.pipe = init_haystack_fqa_pipe(index_name=index_name, model_name=model_name)
 
+        self.tfidf = analyse.extract_tags
+
     def pre_process_text(self, text):
         self.logger.info(f"text:{text}")
+        keywords = self.tfidf(text, allowPOS=["n", "v", "a", "d"])
+        keywords_text = "".join(keywords)
+        text += keywords_text * 3
         return text
 
-    def __call__(self, text):
-        text = self.pre_process_text(text)
-
-        predictions = self.pipe.run(query=text, params={"Retriever": {"top_k": 10}})["answers"]
-        # for prediction in predictions:
-        return_prediction = predictions[0]
-
-        score = return_prediction.to_dict()["score"]
-        meta = return_prediction.meta
-        query = meta["query"]
-        answer = meta["answer"]
-        # source = meta["source"]
-        self.logger.debug(f"query:{query},score:{score}")
+    def post_process_result(self, predictions, query_type=None):
+        answer = "您的问题法域通暂时还理解不了，请您换个说法。"
+        for prediction in predictions:
+            score = prediction.to_dict()["score"]
+            meta = prediction.meta
+            query = meta["query"]
+            answer = meta["answer"]
+            source = meta["type"]
+            self.logger.success(f"query:{query},score:{score},source:{source}")
+            break
 
         similarity_question = [{"question": p.meta["query"], "answer": p.meta["answer"]} for p in predictions]
         similarity_question_info = [{"question": p.meta["query"], "score": p.to_dict()["score"]} for p in predictions]
         self.logger.info(f"similarity_question:{similarity_question_info}")
+
+        return answer, similarity_question
+
+    def __call__(self, text, query_type=None):
+        text = self.pre_process_text(text)
+
+        predictions = self.pipe.run(query=text, params={"Retriever": {"top_k": 10}})["answers"]
+
+        answer, similarity_question = self.post_process_result(predictions, query_type)
+
         return answer, similarity_question
 
 
--- OnlineServer/IntelligentConsultation/server.py
+++ OnlineServer/IntelligentConsultation/server.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 2022/9/26 08:57
-# @Author  : inamori1932
+# @Time    : 2022/10/13 10:23
+# @Author  : Adolf
 # @Site    : 
 # @File    : server.py
 # @Software: PyCharm
 import uvicorn
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from IntelligentConsultation.service_use import consultation_service
 
 app = FastAPI()
 
 
 class QueryInput(BaseModel):
-    question: str = "公司交不起税了怎么办？"
-    source_end: str = None
+    question: str = Field(default="公司交不起税怎么办", description="用户输入的问题")
+    source_end: str = Field(default=None, description="用户输入的问题")
+    query_type: str = Field(default=None, description="选择用户输入的问题类型")
 
 
 @app.post("/get_query_answer")
 def _get_query_answer(query_input: QueryInput):
     """
-    获取线上咨询的答案
-    :param query_input:
-    :return:
+    获取线上智能咨询的结果
     """
     print("input:")
     print("question{}:".format(query_input.question))
     print("source_end{}:".format(query_input.source_end))
+    print("query_type{}:".format(query_input.query_type))
 
     return consultation_service.get_query_answer(query_input.question)
 
@@ -35,16 +35,26 @@ def _get_query_answer(query_input: QueryInput):
 @app.post("/get_query_answer_with_source")
 def _get_query_answer_with_source(query_input: QueryInput):
     """
-    获取专题咨询的内容
-    :param query_input:
-    :return:
+    获取智能咨询的结果
+    参数设定；
+    @question: 用户的问题输入
+    @source_end: 个人端/企业端/律师端
+    @query_type: old/topic/market
     """
     print("input:")
     print("question{}:".format(query_input.question))
     print("source_end{}:".format(query_input.source_end))
+    print("query_type{}:".format(query_input.query_type))
 
-    return consultation_service.get_query_answer_with_source(query_input.question)
+    if query_input.query_type == "old":
+        return consultation_service.get_query_answer(query_input.question)
+
+    else:
+        return consultation_service.get_query_answer_with_source(query_input.question,
+                                                                 query_type=query_input.query_type)
 
 
 if __name__ == '__main__':
-    uvicorn.run('OnlineServer.IntelligentConsultation.server:app', host="0.0.0.0", port=8130, reload=False, workers=1)
+    # 真实使用8130端口。测试使用8131端口
+    # uvicorn.run('OnlineServer.IntelligentConsultation.server:app', host="0.0.0.0", port=8130, reload=False, workers=2)
+    uvicorn.run('OnlineServer.IntelligentConsultation.server:app', host="0.0.0.0", port=8131, reload=True, workers=4)
