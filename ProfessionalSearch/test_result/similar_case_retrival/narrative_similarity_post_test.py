7d6fd3a5d50f744a84f79ba68611a9f5dd7b481f
hyl2048 <2745506937@qq.com>
2022-10-18 11:54:08 +0800

    1.类案人名脱敏util；2.文档编写；3.代码整理

--- ProfessionalSearch/test_result/similar_case_retrival/narrative_similarity_post_test.py
+++ ProfessionalSearch/test_result/similar_case_retrival/narrative_similarity_post_test.py
@@ -2,13 +2,18 @@ import time
 import sys
 import requests
 
-t1 = time.time()
-fact = "借钱不还"
-payload = {"problem": "民间借贷纠纷", "claim_list": [], "fact": fact}
-r = requests.post(
-    "http://172.19.82.199:8163/top_k_similar_narrative", json=payload
-)  # 47.99.90.181
-t2 = time.time()
+from Utils import print_run_time
 
-print(r.text)
-print(t2 - t1)
+
+@print_run_time
+def similar_case_bug():
+    fact = "借钱不还"
+    payload = {"problem": "民间借贷纠纷", "claim_list": [], "fact": fact}
+    r = requests.post(
+        "http://172.19.82.199:8163/top_k_similar_narrative", json=payload
+    )  # 47.99.90.181
+    print(r.text)
+
+
+if __name__ == '__main__':
+    similar_case_bug()
