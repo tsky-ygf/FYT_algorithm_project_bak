92957f1caad035324ccae505f6a1b6c41610234a
yuguofeng <yu_guofeng@nblhtech.com>
2022-10-19 17:28:46 +0800

    update CommoonLaws code

--- OnlineServer/CommonLaws/app_test.py
+++ OnlineServer/CommonLaws/app_test.py
@@ -5,3 +5,8 @@
 # @Site    : 
 # @File    : app_test.py
 # @Software: PyCharm
+import requests
+import json
+response = requests.post("http://127.0.0.1:7000/getNews", json={"category": "税法专栏"})
+result = response.json()
+print(result)
