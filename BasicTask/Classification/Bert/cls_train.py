1e4008e15c1cae6091483a69884beb3b1f6d066c
adolf <adolf1321794021@gmail.com>
2022-10-19 09:44:47 +0800

    update

--- BasicTask/Classification/Bert/cls_train.py
+++ BasicTask/Classification/Bert/cls_train.py
@@ -14,12 +14,6 @@ from transformers import AutoConfig, AutoTokenizer, AutoModelForSequenceClassifi
 
 
 class TrainClassification(BaseTrainTool):
-    def __init__(self, config, create_examples):
-        super(TrainClassification, self).__init__(config=config, create_examples=create_examples)
-        self.criterion = torch.nn.BCELoss()
-
-    # self.create_examples = self
-
     def init_model(self):
         tokenizer = AutoTokenizer.from_pretrained(self.config["pre_train_tokenizer"])
         # model = MultiLabelClsModel(self.config)
@@ -29,24 +23,12 @@ class TrainClassification(BaseTrainTool):
             config=model_config,
             from_tf=False,
         )
-        # self.logger.debug(model)
-
-        # for param in model.longformer.parameters():
-        #     param.requires_grad = False
 
         return tokenizer, model
 
-    def cal_loss(self, batch):
-        self.logger.debug(batch)
-        labels = batch['labels']
-        # input_data = {'input_ids': batch['input_ids'],
-        #               'atention_mask': batch['attention_mask'],
-        #               'token_type_ids': batch['token_type_ids']}
-        pred = self.model(batch['input_ids'].squeeze())
-        loss = self.criterion(torch.sigmoid(pred.logits), labels.squeeze())
-        self.logger.debug(loss)
-        return loss
+    def create_examples(self, data_path, mode):
+        pass
 
-# if __name__ == '__main__':
-#     TrainClassification(config_path="BasicTask/Classification/Bert/base_cls_train.yaml").train_main()
-#     # TrainLawsCls(config_path="RelevantLaws/Config_bak/base_cls_train.yaml").eval_epoch()
+
+if __name__ == '__main__':
+    TrainClassification(config_path="BasicTask/Classification/Bert/base_cls_train.yaml").run()
