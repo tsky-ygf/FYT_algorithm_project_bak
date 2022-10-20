1e4ccd20a90d8ed56232257366f720599930ca67
adolf <adolf1321794021@gmail.com>
2022-10-20 14:01:13 +0800

    update

--- OnlineServer/IntelligentConsultation/server.py
+++ OnlineServer/IntelligentConsultation/server.py
@@ -13,7 +13,6 @@ from IntelligentConsultation.service_use import consultation_service
 # from fastapi.exceptions import RequestValidationError
 # from fastapi.responses import JSONResponse
 
-# from typing import Optional
 
 app = FastAPI()
 
@@ -26,7 +25,6 @@ app = FastAPI()
 
 class QueryInput(BaseModel):
     question: str = Field(default="公司交不起税怎么办", description="用户输入的问题")
-    # source_end: str = Field(default="通用", description="用户使用的客户端")
     query_type: str = Field(default="专题", description="选择一级主题分类，可选项:(旧版/专题)")
     query_sub_type: str = Field(default="通用",
                                 description="选择二级主题分类，可选项:(通用/法院/公安/环保/交通/金融/科技/市场监管/税务/司法/文旅)")
@@ -41,31 +39,35 @@ class QueryInput(BaseModel):
         }
 
 
-@app.post("/get_query_answer_with_source")
+class QueryResult(BaseModel):
+    answer: str
+    similarity_question: list[dict]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "answer": "回答内容",
+                "similarity_question": [
+                    {"question": "相似问题1", "answer": "回答内容1"},
+                ]
+            }
+        }
+
+
+@app.post("/get_query_answer_with_source", response_model=QueryResult)
 def _get_query_answer_with_source(query_input: QueryInput):
     """
-    获取智能咨询的结果
-
-    请求参数:
-
-    | Param           | Type | Description                                       |
-    |-----------------|------|---------------------------------------------------|
-    | question        | str  | 用户输入的问题                                           |
-    | query_type      | str  | 选择一级主题分类，可选项:(旧版/专题)                              |
-    | query_sub_type  | str  | 选择二级主题分类，可选项:(通用/法院/公安/环保/交通/金融/科技/市场监管/税务/司法/文旅) |
-
-    响应参数:
-
-    | Param               | Type           | Description |
-    |---------------------|----------------|-------------|
-    | answer              | str            | 智能咨询的答案     |
-    | similarity_question | Optional[List] | 返回的相似问题     |
+    获取智能咨询的结果和相似问答
     """
-    return consultation_service.get_answer_for_question(question=query_input.question,
-                                                        query_type=query_input.query_type,
-                                                        query_sub_type=query_input.query_sub_type)
+    result = consultation_service.get_answer_for_question(question=query_input.question,
+                                                          query_type=query_input.query_type,
+                                                          query_sub_type=query_input.query_sub_type)
+    # response_model.answer = result.get("answer", "")
+    # response_model.similarity_question = result.get("similarity_question", [])
+
+    return result
 
 
 if __name__ == '__main__':
     # 真实使用8134端口
-    uvicorn.run('OnlineServer.IntelligentConsultation.server:app', host="0.0.0.0", port=8134, reload=False, workers=2)
+    uvicorn.run('OnlineServer.IntelligentConsultation.server:app', host="0.0.0.0", port=8134, reload=True, workers=1)
