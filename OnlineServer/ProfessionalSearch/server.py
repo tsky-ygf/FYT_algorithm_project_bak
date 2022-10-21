22db398a558ae3984b594b128d4d2604a19cd8bd
company <huang_yulin@nblhtech.com>
2022-10-21 14:17:39 +0800

    检索接口文档修改

--- OnlineServer/ProfessionalSearch/server.py
+++ OnlineServer/ProfessionalSearch/server.py
@@ -227,41 +227,41 @@ async def search_laws(search_query: Search_laws_input) -> dict:
 
     请求参数:
 
-        | Param             | Type | Description          |
-        |-------------------|------|----------------------|
-        | query             | str  | 用户输入的查询内容            |
-        | filter_conditions | Dict | 用户输入的条件，小于就返回搜索结果的长度 |
-        | page_number       | int  | 第几页                  |
-        | page_size         | int  | 页大小                  |
+    | Param             | Type | Description          |
+    |-------------------|------|----------------------|
+    | query             | str  | 用户输入的查询内容            |
+    | filter_conditions | Dict | 用户输入的条件，小于就返回搜索结果的长度 |
+    | page_number       | int  | 第几页                  |
+    | page_size         | int  | 页大小                  |
 
-        filter_conditions的内容如下:
+    filter_conditions的内容如下:
 
-        * timeliness: List[str], 时效性，可选项（全部/有效/已修改/尚未生效/已废止）
+    * timeliness: List[str], 时效性，可选项（全部/有效/已修改/尚未生效/已废止）
 
-        * types_of_law: List[str], 法条种类，可选项（全部/宪法/法律/行政法规/监察法规/司法解释/地方性法规 ）
+    * types_of_law: List[str], 法条种类，可选项（全部/宪法/法律/行政法规/监察法规/司法解释/地方性法规 ）
 
-        * scope_of_use: List[str], 地域，可选项（全国/安徽省/北京市/重庆市/福建省/甘肃省/广东省/
-          广西壮族自治区/贵州省/海南省/河北省/河南省/黑龙江省/湖北省/湖南省/吉林省/江苏省/江西省/
-          辽宁省/内蒙古自治区/宁夏回族自治区/青海省/山东省/山西省/陕西省/上海市/四川省/天津市/
-          西藏自治区/新疆维吾尔自治区/云南省/浙江省）
+    * scope_of_use: List[str], 地域，可选项（全国/安徽省/北京市/重庆市/福建省/甘肃省/广东省/
+      广西壮族自治区/贵州省/海南省/河北省/河南省/黑龙江省/湖北省/湖南省/吉林省/江苏省/江西省/
+      辽宁省/内蒙古自治区/宁夏回族自治区/青海省/山东省/山西省/陕西省/上海市/四川省/天津市/
+      西藏自治区/新疆维吾尔自治区/云南省/浙江省）
     响应参数:
 
-        | Param        | Type       | Description                      |
-        |--------------|------------|----------------------------------|
-        | laws         | List[dict] | 返回的法律list集合                      |
-        | total_amount | int        | 若搜索的结果有200条以上则返回200，小于就返回搜索结果的长度 |
+    | Param        | Type       | Description                      |
+    |--------------|------------|----------------------------------|
+    | laws         | List[dict] | 返回的法律list集合                      |
+    | total_amount | int        | 若搜索的结果有200条以上则返回200，小于就返回搜索结果的长度 |
 
-        laws的内容如下:
+    laws的内容如下:
 
-        *   law_id:        str,    数据库表名 + 分隔符 + 法条的md5Clause
-        *   law_name:      str,    标题
-        *   law_type:      str,    国家法律法规网页标题
-        *   timeliness:    str,    时效性
-        *   using_range:   str,    省份
-        *   law_chapter:   str,    章
-        *   law_item:      str,    节
-        *   result:        str,    条
-        *   law_content:   str,    返回具体法律内容
+    *   law_id:        str,    数据库表名 + 分隔符 + 法条的md5Clause
+    *   law_name:      str,    标题
+    *   law_type:      str,    国家法律法规网页标题
+    *   timeliness:    str,    时效性
+    *   using_range:   str,    省份
+    *   law_chapter:   str,    章
+    *   law_item:      str,    节
+    *   result:        str,    条
+    *   law_content:   str,    返回具体法律内容
     """
     try:
         result = _get_law_result(
@@ -284,54 +284,54 @@ async def search_laws(search_query: Search_laws_input) -> dict:
 @app.get("/get_filter_conditions_of_case", response_model=conditions_case_result)
 async def _get_filter_conditions() -> dict:
     """
-        返回案例检索的过滤条件
+    返回案例检索的过滤条件
 
-        请求参数:
+    请求参数:
 
-            无
+    无
 
-        响应参数:
+    响应参数:
 
-            | Param            | Type | Description |
-            |------------------|------|-------------|
-            | type_of_case     | Dict | 案例检索的过滤条件   |
-            | court_level      | Dict | 案例检索的过滤条件   |
-            | type_of_document | Dict | 案例检索的过滤条件   |
-            | region           | Dict | 案例检索的过滤条件   |
+    | Param            | Type | Description |
+    |------------------|------|-------------|
+    | type_of_case     | Dict | 案例检索的过滤条件   |
+    | court_level      | Dict | 案例检索的过滤条件   |
+    | type_of_document | Dict | 案例检索的过滤条件   |
+    | region           | Dict | 案例检索的过滤条件   |
 
-            type_of_case的内容如下:
-              * name: str, 案件类型
+    type_of_case的内容如下:
+      * name: str, 案件类型
 
-              * is_multiple_choice: boolean, 是否支持多选
+      * is_multiple_choice: boolean, 是否支持多选
 
-              * value: List[str], 案件类型的选项，选项（全部/刑事/民事/行政/执行）
+      * value: List[str], 案件类型的选项，选项（全部/刑事/民事/行政/执行）
 
-            court_level的内容如下:
+    court_level的内容如下:
 
-              * name: str, 法院层级
+      * name: str, 法院层级
 
-              * is_multiple_choice: boolean, 是否支持多选
+      * is_multiple_choice: boolean, 是否支持多选
 
-              * value: List[str], 法院层级的选项，选项（全部/最高/高级/中级/基层）
+      * value: List[str], 法院层级的选项，选项（全部/最高/高级/中级/基层）
 
-            type_of_document的内容如下:
+    type_of_document的内容如下:
 
-              * name: str, 文书类型
+      * name: str, 文书类型
 
-              * is_multiple_choice: boolean, 是否支持多选
+      * is_multiple_choice: boolean, 是否支持多选
 
-              * value: List[str], 文书类型的选项，选项（全部/判决/裁定/调解）
+      * value: List[str], 文书类型的选项，选项（全部/判决/裁定/调解）
 
-            region的内容如下:
+    region的内容如下:
 
-              * name: str, 地域
+      * name: str, 地域
 
-              * is_multiple_choice: boolean, 是否支持多选
+      * is_multiple_choice: boolean, 是否支持多选
 
-              * value: List[str], 地域的选项，选项（全国/安徽省/北京市/重庆市/福建省/甘肃省/广东省/
-              广西壮族自治区/贵州省/海南省/河北省/河南省/黑龙江省/湖北省/湖南省/吉林省/江苏省/江西省/
-              辽宁省/内蒙古自治区/宁夏回族自治区/青海省/山东省/山西省/陕西省/上海市/四川省/天津市/
-              西藏自治区/新疆维吾尔自治区/云南省/浙江省）
+      * value: List[str], 地域的选项，选项（全国/安徽省/北京市/重庆市/福建省/甘肃省/广东省/
+      广西壮族自治区/贵州省/海南省/河北省/河南省/黑龙江省/湖北省/湖南省/吉林省/江苏省/江西省/
+      辽宁省/内蒙古自治区/宁夏回族自治区/青海省/山东省/山西省/陕西省/上海市/四川省/天津市/
+      西藏自治区/新疆维吾尔自治区/云南省/浙江省）
     """
     return get_filter_conditions_of_case()
 
@@ -339,44 +339,44 @@ async def _get_filter_conditions() -> dict:
 @app.post("/search_cases", response_model=search_cases_result)
 async def search_cases(search_query: Search_cases_input) -> dict:
     """
-        获取法律检索的结果
+    获取法律检索的结果
 
-        请求参数:
+    请求参数:
 
-            | Param             | Type | Description |
-            |-------------------|------|-------------|
-            | query             | str  | 用户输入的查询内容   |
-            | filter_conditions | Dict | 用户输入的条件     |
-            | page_number       | int  | 第几页         |
-            | page_size         | int  | 页大小         |
+    | Param             | Type | Description |
+    |-------------------|------|-------------|
+    | query             | str  | 用户输入的查询内容   |
+    | filter_conditions | Dict | 用户输入的条件     |
+    | page_number       | int  | 第几页         |
+    | page_size         | int  | 页大小         |
 
-            filter_conditions的内容如下:
+    filter_conditions的内容如下:
 
-            * type_of_case: List[str], 案件类型，可选项（全部/刑事/民事/行政/执行）
+    * type_of_case: List[str], 案件类型，可选项（全部/刑事/民事/行政/执行）
 
-            * court_level: List[str], 法院层级，可选项（全部/最高/高级/中级/基层）
+    * court_level: List[str], 法院层级，可选项（全部/最高/高级/中级/基层）
 
-            * type_of_document: List[str], 文书类型，可选项（全部/判决/裁定/调解）
+    * type_of_document: List[str], 文书类型，可选项（全部/判决/裁定/调解）
 
-            * region: List[str], 地域，可选项（全国/安徽省/北京市/重庆市/福建省/甘肃省/广东省/
-              广西壮族自治区/贵州省/海南省/河北省/河南省/黑龙江省/湖北省/湖南省/吉林省/江苏省/江西省/
-              辽宁省/内蒙古自治区/宁夏回族自治区/青海省/山东省/山西省/陕西省/上海市/四川省/天津市/
-              西藏自治区/新疆维吾尔自治区/云南省/浙江省）
+    * region: List[str], 地域，可选项（全国/安徽省/北京市/重庆市/福建省/甘肃省/广东省/
+      广西壮族自治区/贵州省/海南省/河北省/河南省/黑龙江省/湖北省/湖南省/吉林省/江苏省/江西省/
+      辽宁省/内蒙古自治区/宁夏回族自治区/青海省/山东省/山西省/陕西省/上海市/四川省/天津市/
+      西藏自治区/新疆维吾尔自治区/云南省/浙江省）
 
-        响应参数:
+    响应参数:
 
-            | Param        | Type       | Description                      |
-            |--------------|------------|----------------------------------|
-            | cases        | List[dict] | 返回案例的list集合                      |
-            | total_amount | int        | 若搜索的结果有200条以上则返回200，小于就返回搜索结果的长度 |
+    | Param        | Type       | Description                      |
+    |--------------|------------|----------------------------------|
+    | cases        | List[dict] | 返回案例的list集合                      |
+    | total_amount | int        | 若搜索的结果有200条以上则返回200，小于就返回搜索结果的长度 |
 
-            cases的内容如下:
+    cases的内容如下:
 
-            * doc_id:        str,  数据库表名 + 分隔符 + 裁判文书的uq_id
-            * court:         str,  法院名称
-            * case_number:   str,  文书号
-            * jfType:        str,  纠纷类型
-            * content:       str,  裁判文书内容
+    * doc_id:        str,  数据库表名 + 分隔符 + 裁判文书的uq_id
+    * court:         str,  法院名称
+    * case_number:   str,  文书号
+    * jfType:        str,  纠纷类型
+    * content:       str,  裁判文书内容
     """
     try:
         if search_query is not None:
@@ -407,24 +407,24 @@ async def search_cases(search_query: Search_cases_input) -> dict:
 @app.post("/top_k_similar_narrative", response_model=similar_narrative_result)
 async def get_similar_case(search_query: Similar_case_input) -> dict:
     """
-        返回相似类案的结果
+    返回相似类案的结果
 
-        请求参数:
+    请求参数:
 
-            | Param      | Type | Description                                                                                                                                                                                                                                                                                                                                                                                                                                               |
-            |------------|------|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
-            | fact       | str  | 用户输入的事实描述                                                                                                                                                                                                                                                                                                                                                                                                                                                 |
-            | problem    | str  | 用户输入的纠纷类型，可选项:(婚姻家庭/继承纠纷/子女抚养/老人赡养/返还彩礼/财产分割/同居问题/工伤赔偿/社保纠纷/劳动纠纷/劳务纠纷/劳务人员受害/劳务人员致害/用人单位责任/民间借贷/金融借贷/交通事故纠纷/人身伤害赔偿/财产损失赔偿/保险赔付/医药费赔偿/误工费赔偿/违章扣分/合伙纠纷/股权转让/买卖合同/租赁合同/运输合同/赠与合同/承揽合同/委托合同/保证合同/抵押合同/技术服务合同/技术开发合同/技术转让合同/技术咨询合同/中介合同/融资租赁合同/不当得利/善意取得/房屋买卖/物业服务/仓储合同/相邻关系/业主权/网络侵权/隐私权和个人信息保护/名誉权与荣誉权/肖像权/姓名权/动物致人损害/触电人身损害/道路施工损害/道路通行损害/建筑倒塌损害/物件脱落损害/高空抛物/医疗损害/公共场所损害/监护人责任/教育机构责任/人身损害/消费者权利保护/产品责任/建设用地使用权/建设工程合同/土地承包/宅基地使用权/著作权纠纷/专利权纠纷/商标权纠纷/不正当竞争) |
-            | claim_list | List | 诉求类型                                                                                                                                                                                                                                                                                                                                                                                                                                                      |
+    | Param      | Type | Description                                                                                                                                                                                                                                                                                                                                                                                                                                               |
+    |------------|------|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+    | fact       | str  | 用户输入的事实描述                                                                                                                                                                                                                                                                                                                                                                                                                                                 |
+    | problem    | str  | 用户输入的纠纷类型，可选项:(婚姻家庭/继承纠纷/子女抚养/老人赡养/返还彩礼/财产分割/同居问题/工伤赔偿/社保纠纷/劳动纠纷/劳务纠纷/劳务人员受害/劳务人员致害/用人单位责任/民间借贷/金融借贷/交通事故纠纷/人身伤害赔偿/财产损失赔偿/保险赔付/医药费赔偿/误工费赔偿/违章扣分/合伙纠纷/股权转让/买卖合同/租赁合同/运输合同/赠与合同/承揽合同/委托合同/保证合同/抵押合同/技术服务合同/技术开发合同/技术转让合同/技术咨询合同/中介合同/融资租赁合同/不当得利/善意取得/房屋买卖/物业服务/仓储合同/相邻关系/业主权/网络侵权/隐私权和个人信息保护/名誉权与荣誉权/肖像权/姓名权/动物致人损害/触电人身损害/道路施工损害/道路通行损害/建筑倒塌损害/物件脱落损害/高空抛物/医疗损害/公共场所损害/监护人责任/教育机构责任/人身损害/消费者权利保护/产品责任/建设用地使用权/建设工程合同/土地承包/宅基地使用权/著作权纠纷/专利权纠纷/商标权纠纷/不正当竞争) |
+    | claim_list | List | 诉求类型                                                                                                                                                                                                                                                                                                                                                                                                                                                      |
 
-        响应参数:
+    响应参数:
 
-            | Param       | Type        | Description |
-            |-------------|-------------|-------------|
-            | dids        | List[str]   | 裁判文书的uq_id  |
-            | sims        | List[float] | 相似类案的相似率    |
-            | reasonNames | List[str]   | 纠纷类型        |
-            | tags        | List[str]   | 关键词         |
+    | Param       | Type        | Description |
+    |-------------|-------------|-------------|
+    | dids        | List[str]   | 裁判文书的uq_id  |
+    | sims        | List[float] | 相似类案的相似率    |
+    | reasonNames | List[str]   | 纠纷类型        |
+    | tags        | List[str]   | 关键词         |
     """
     try:
         if search_query is not None:
@@ -464,19 +464,19 @@ async def get_similar_case(search_query: Similar_case_input) -> dict:
 @app.post("/desensitization", response_model=desensitization_result)
 async def get_text_desen(dese_in: desensitization_input) -> dict:
     """
-        返回人名脱敏的结果
+    返回人名脱敏的结果
 
-        请求参数:
+    请求参数:
 
-            | Param | Type | Description |
-            |-------|------|-------------|
-            | text  | str  | 待脱敏的文本      |
+    | Param | Type | Description |
+    |-------|------|-------------|
+    | text  | str  | 待脱敏的文本      |
 
-        响应参数:
+    响应参数:
 
-            | Param  | Type | Description |
-            |--------|------|-------------|
-            | result | str  | 人名脱敏后的文本    |
+    | Param  | Type | Description |
+    |--------|------|-------------|
+    | result | str  | 人名脱敏后的文本    |
     """
     return {"res": desensitization(dese_in.text)}
 
