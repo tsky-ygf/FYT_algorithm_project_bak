5e4310430aae927bcdd15ff2f674161ea1f6a61d
yuguofeng <yu_guofeng@nblhtech.com>
2022-10-21 09:24:16 +0800

    update CommonLaws

--- OnlineServer/CommonLaws/server.py
+++ OnlineServer/CommonLaws/server.py
@@ -36,6 +36,27 @@ class CategoryResult(BaseModel):
 
 @app.post("/get_preview_commonLaws_data", response_model=CategoryResult)
 async def get_example_model_data(category: CategoryInput):
+    """
+        获取 专栏预览 信息
+
+        请求参数:
+
+        | Param           | Type | Description                                       |
+        |-----------------|------|---------------------------------------------------|
+        | category        | str  | 专栏名称->可选项:(税法专栏/司法专栏/金融专栏/市场监督/法院专栏/公安专栏/文旅专栏/环保专栏/交通专栏/科技专栏)                                         |
+
+        响应参数:
+
+        | Param       | Type           | Description |
+        |-------------|----------------|-------------|
+        | uq_id       | str            | 主键 ID      |
+        | title       | str            | 标题         |
+        | pubDate     | str            | 发布时间      |
+        | source      | str            | 来源          |
+        | preview     | str            | 预览内容信息   |
+        | tableName   | str            | 数据库表名     |
+
+        """
     tabName = common_laws_service.get_table(category.category)
     preview_data_list = common_laws_service.get_preview_data(tableName=tabName)
     return {"data_list":preview_data_list}
@@ -48,7 +69,7 @@ class NewsInput(BaseModel):
         schema_extra = {
             "example": {
                 "table_name": "swj_hot_news",
-                "query_type": "e9b672a97cf1d68e57d1cbcd38f6237f",
+                "uq_id": "e9b672a97cf1d68e57d1cbcd38f6237f",
             }
         }
 
@@ -57,6 +78,28 @@ class NewsResult(BaseModel):
 
 @app.post("/get_commonLaws_news_by_id", response_model=NewsResult)
 async def get_news(info_input: NewsInput):
+    """
+        根据 ID 查询专栏详细信息
+
+        请求参数:
+
+        | Param           | Type | Description                                       |
+        |-----------------|------|---------------------------------------------------|
+        | table_name      | str  | 数据库表 名称                                           |
+        | uq_id           | str  | 数据库表 主键ID                            |
+
+        响应参数:
+
+        | Param               | Type           | Description |
+        |---------------------|----------------|-------------|
+        | url                 | str            | 网站链接     |
+        | htmlContent         | str            | 带HTML的原文内容     |
+        | title               | str            | 标题     |
+        | pubDate             | str            | 发布时间     |
+        | source              | str            | 来源     |
+        | preview             | str            | 内容预览     |
+
+        """
     data_list = common_laws_service.get_news(uq_id=info_input.uq_id,
                                              tableName=info_input.table_name)
     return {"data_list":data_list}
